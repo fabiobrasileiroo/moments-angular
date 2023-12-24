1a9ead19d7cfcd41ca751baa81bbdbffcdec8788
fabiobrasileiroo <fabio.h591@gmail.com>
2023-12-23 21:18:33 -0400

    adicionando detalhes ao post

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Moment } from '../Moment';
+import { Response } from '../Response'
 
 import  { environment } from 'src/environments/environment' 
 
@@ -11,8 +12,18 @@ import  { environment } from 'src/environments/environment'
 })
 export class MomentService {
   private baseApiUrl = environment.baseApiUrl
-  private apiUrl =  `${this.baseApiUrl}api/moments`
-  constructor(private http: HttpClient) { }
+  private apiUrl =  `${this.baseApiUrl}api/moments`;
+
+  constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl)
+  }
+  
+  getMoment(id: number): Observable<Response<Moment>> {
+    const url = `${this.apiUrl}/${id}`
+    return this.http.get<Response<Moment>>(url)
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData)
